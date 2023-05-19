b57a360e3e02753d8baa10b480910c077152bb01
wawanpi <muhaguskurniawan@gmail.com>
2023-05-19 11:02:44 +0700

    menampilkan nim nama dan mhs

--- SingleLinkedList_202/SingleLinkedList_202.cpp
+++ SingleLinkedList_202/SingleLinkedList_202.cpp
@@ -12,5 +12,14 @@ Node* START = NULL;
 
 void addNode()
 {
-	 
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan Nim: ";
+	cin >> nim;
+	cout << "Masukan Nama:";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
 }
