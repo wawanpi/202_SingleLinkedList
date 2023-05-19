7aa436c1d0708886a07ae8358891a6dc0b50f18c
wawanpi <muhaguskurniawan@gmail.com>
2023-05-19 11:12:35 +0700

    menampilkan nomhs

--- SingleLinkedList_202/SingleLinkedList_202.cpp
+++ SingleLinkedList_202/SingleLinkedList_202.cpp
@@ -34,8 +34,17 @@ void addNode()
 		while ((current != NULL) && (nim >= current->noMhs))
 		{
 			if (nim == current->noMhs) {
-				cout <<
+				cout << "NIM sudah ada" << endl;
+				return;
 			}
+
+			previous = current;
+			current = current->next;
 		}
+
+		nodeBaru->next = current;
+		previous->next = nodeBaru;
 	}
 }
+
+bol
